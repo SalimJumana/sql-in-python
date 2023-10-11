32d633a1eb257087f1d9299d8a6eabf6618dedf2
SalimJumana <135883107+SalimJumana@users.noreply.github.com>
2023-10-12 02:55:22 +0530

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,11 +1,39 @@
 # test if a word is a palindrome
+
+# 1) using list and reversed() function
 def is_palindrome(word):
     word = word.strip().lower()
     rev_word = reversed(word)
+    # to print a reversed object, we have to convert it to a list
 
     if list(word) == list(rev_word):
         print(True)
     else:
         print(False)
 
+# 2) Join and reversed() function
+    # join string will be kept empty("") to avoid anything in between items in "word" in our new string
+    if word == "".join(rev_word):
+        print(True)
+    else:
+        print(False)
+
 is_palindrome("Apple ")
+
+# 3) using list and .reverse() method
+def is_palindrome_2(word):
+    word = list(word.strip().lower())
+    rev_word = list(word)
+    # rev_word = word.copy();   both of these ways create a copy list of the given word
+    rev_word.reverse()
+
+    if word == rev_word:
+        print(True)
+    else:
+        print(False)
+# 4) using slices
+    if word == word[::-1]:
+        print(True)
+    else: print(False)
+is_palindrome_2("Apple ")
+is_palindrome_2("Golog ")
